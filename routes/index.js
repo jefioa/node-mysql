3ee23ae7c4c878da6e13b8a148d4bb8d17a2ac85
jefioa <cte117jau@icloud.com>
2026-06-18 07:20:01 +0000

    chapter 14完成

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
 
-let todos = [];
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'todo_app'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('MySQL connection error:', err.stack || err);
+    return;
+  }
+  console.log('MySQL connected as id', connection.threadId);
+});
 
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'ToDo App',
-    todos: todos,
-  });
+  connection.query(
+    'select * from tasks;',
+    (error, results) => {
+      if (error) {
+        return next(error);
+      }
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results,
+      });
+    }
+  );
 });
 
 router.post('/', function (req, res, next) {
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/');
+  connection.query(
+    'insert into tasks (user_id, content) values (1, ?);',
+    [todo],
+    (error, results) => {
+      if (error) {
+        return next(error);
+      }
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
